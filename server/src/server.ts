5099bf6df677c9a4754572363af8b84a4259b5ff
Ian Goldfarb <ian@supplyhub.com>
2018-03-19 15:49:43 -0700

    fixed cors issue

--- server/src/server.ts
+++ server/src/server.ts
@@ -183,18 +183,15 @@ export class ChatServer {
   private changeApiAccess() {
     // Add headers
     this.app.use(function (req, res, next) {
-
-      // Website you wish to allow to connect
-      res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
-
-      // Request methods you wish to allow
-      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-      // Request headers you wish to allow
-      res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-      // Pass to next layer of middleware
-      next();
+      let allowedOrigins = ['http://socket-questions.bitballoon.com', 'http://localhost:4200'];
+      let origin = req.headers.origin;
+      if (allowedOrigins.indexOf(origin) > -1) {
+        res.setHeader('Access-Control-Allow-Origin', origin);
+      }
+      res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+      res.header('Access-Control-Allow-Credentials', true);
+      return next();
     });
   }
 
